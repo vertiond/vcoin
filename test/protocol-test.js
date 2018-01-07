2ff66049448df9585fc5848ea1f47a9de0f919c1
Thomas L <Cubey2019@users.noreply.github.com>
2018-01-07 02:56:09 +0000

    Reverted previous change to pool.js that allowed data to be downloaded during the initial sync (appears to require more changes for this to work correctly) Updated user agent to report vcoin instead of bcoin.

--- test/protocol-test.js
+++ test/protocol-test.js
@@ -20,7 +20,7 @@ const tx9 = common.readTX('tx9');
 
 describe('Protocol', function() {
   const pkg = require('../lib/pkg');
-  const agent = `/bcoin:${pkg.version}/`;
+  const agent = `/vcoin:${pkg.version}/`;
   let parser, framer;
 
   beforeEach(() => {
